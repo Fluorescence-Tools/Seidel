773338ce4d7fab49f42e320c88d8bab039eb4589
Voort <voort@AD.hhu.de>
2019-06-27 15:00:23 +0200

    added c wrapper for fit2DRoutine. Wrote MLE based fitting and simulation for 2D Gauss

--- Code/cpp_wrappers.py
+++ Code/cpp_wrappers.py
@@ -9,7 +9,6 @@ import numpy as np
 import ctypes
 import os
 
-
 readPTU = ctypes.WinDLL (r"S:\64bit dll 's\PQ_PTU_sf\release\PQ_PTU.dll")
 #_SplitOnTacs = ctypes.CDLL(r'K:\vanderVoortN\FRC\dev\readPTU\ProcessPhotonStream.dll').SplitOnTacs
 debug = False
@@ -116,3 +115,71 @@ def SplitOnTacs_wrap(eventN, tac, t, can, dimX, dimY, dwelltime, counttime, NumR
     imA = imA.reshape((dimX, dimY))
     imB = imB.reshape((dimX, dimY))
     return imA, imB
+
+def fit2DGaussian_wrap(params0, im, a):
+    """wrapper to fit2DGaussian function from cpp used in Ani Fitting routine
+    params0: initial guess for parameters. They are ordered according to Mortensen et al.
+    im: 2D Gauss image to be fitted
+    a: pixel size"""
+    
+    #ISSUE: class declaration occurs inside of function, this disables use outside of current
+    #function. It cannot be changed easily as imsize must be known for class initialisation.
+    
+    fit2DGaussian = ctypes.WinDLL(r"K:\vanderVoortN\FRC\dev\Fit2DGaussian\x64\Release\Fit2DGaussian.dll").fit2DGaussian
+
+    c_double_p = ctypes.POINTER(ctypes.c_double)
+    imsize = im.flatten().shape[0]
+    DOUBLEARRAY = ctypes.c_double * imsize
+
+    class LVDoubleArray(ctypes.Structure):
+        _fields_ = [
+            ('length', ctypes.c_int),#length of array i.e. imsize
+            ('data', DOUBLEARRAY) #image array
+        ]
+
+    class MGPARAM(ctypes.Structure):
+        _fields_ = [
+            ('subimage', ctypes.POINTER(ctypes.POINTER(LVDoubleArray))),
+            ('osize', ctypes.c_int), #number or rows in squared image
+            ('M', ctypes.POINTER(ctypes.POINTER(LVDoubleArray)))
+        ]
+    #give paramaters in correct format
+    variables = np.zeros(9, dtype = np.double).ctypes.data_as(c_double_p)
+    variables[0] = params0[0] / a#ux
+    variables[1] = params0[1] / a#uy
+    variables[2] = params0[4]#N
+    variables[3] = params0[2] / a#sx = s
+    variables[4] = params0[2] / a#sy = s
+    variables[5] = params0[3]#b
+    variables[6] = 0#info from optimization algorithm
+    variables[7] = 0#bool fr weight or no weight: Ask Suren for correct setting
+    variables[8] = 0#bool if background is fitted: 0 -> background is fitted
+
+    c_double_p = ctypes.POINTER(ctypes.c_double)
+    fit2DGaussian.argtypes = [ctypes.POINTER(ctypes.c_double), ctypes.POINTER(MGPARAM)]
+    fit2DGaussian.restypes = ctypes.c_double
+
+    c_im = DOUBLEARRAY()
+    c_M = DOUBLEARRAY()
+
+    for i, el in enumerate(im):
+        c_im[i] = el
+        c_M[i] = 1
+
+    c_imsize = ctypes.c_int(imsize)
+    c_osize = ctypes.c_int(int(np.sqrt(imsize)))
+
+
+    subimage = ctypes.pointer(LVDoubleArray(c_imsize, c_im))
+    subM = ctypes.pointer(LVDoubleArray(c_imsize, c_M))
+    mgparam = MGPARAM(ctypes.pointer(subimage), c_osize, ctypes.pointer(subM))
+
+    fit2DGaussian(variables, mgparam)
+    
+    params = np.zeros(5)
+    params[0] = (variables[0]+0.5) * a
+    params[1] = (variables[1]+0.5) *a
+    params[2] = np.sqrt(variables[3]**2+variables[4]**2) * a
+    params[3] = variables[5]
+    params[4] = variables[2]
+    return params
---
+++ Code/precisionFuncs.py
@@ -0,0 +1,90 @@
+# -*- coding: utf-8 -*-
+"""
+
+Code adapted from Mortensen et. al.
+Nat Methods. 2010 May ; 7(5): 377–381. doi:10.1038/nmeth.1447.
+
+% Maximum likelihood fit to a 2D Gaussian with a constant background
+%
+%   N* 1/(2*pi*s^2) * exp (-( (x-ux).^2+(y-uy).^2 ) / (2*s^2)) + b^2
+%
+% Input Parameters:
+% Required:
+% data -- the image of the isolated probe.
+% params0 -- The user's initial guess of the parameters to be fit:
+%           [ux, uy, s, b, N]
+%           ux, uy and s should be specified in nanometers.
+%           note: as the fit minimizes only localy it is important for this
+%           inital guess to be fairly close to the true value.
+            note 2: this fit optimizes the adjusted s as per eq. 6 suppl. To obtain the FWHM of the PSF,
+            it must be corrected for the pixelsize.
+            ux, uy: center position in nm
+            s: cilindrical sigma in nm
+            b: b^2 background density (cnts / px)
+            N: Total number of signal photons in all pixels
+% a -- pixel size in nanometers
+%
+% Optional:
+% plot_on -- a binary parameter that determines whether the outcome is
+% plotted.
+% find_variance -- a binary parameter that determines whether the variance
+% should be calculated. If find_variance is 0, then -1 is returned as the
+% variance.
+% noise_floor -- a constant offset added by the camera before the pixel is
+% read out. This is necessary if an accurate spot amplitude is desired.
+%
+% Output parameter:
+% paramsF -- the result of the fit.
+%
+% Optional:
+% variance -- the variance of the localization based on eq.(5) of Mortensen
+% et al.
+Added feature: ability to make a single bead image.
+"""
+
+import numpy as np
+from scipy.optimize import minimize
+from scipy.integrate import quad
+
+def twoDGauss(x, y, ux, uy, s):
+    """2D circular Gauss function with integral normalised to one. """
+    p = 1 / (2 * np.pi * s**2) * np.exp(- ( (x-ux)**2 + (y-uy)**2 )/ (2*s**2))
+    return p
+    
+def Expected(x, y, params, a):
+    """calculate the expectation value in point x,y given the gaussian parameters params and pixel size a"""
+    #an alteration to the original MatLab code has been made to account for b being defined as
+    #background density / nm. This was missing in the original code.
+    E = params[4] * a**2 * twoDGauss(x, y, params[0], params[1], params[2]) + params[3]**2
+    return E
+    
+def createGaussImg(dim, params, a):
+    """Relies on the function Expected to create an expectation value image. Then takes Poisson statistics."""
+    x = (np.arange(dim)+0.5) * a
+    y = x
+    x, y = np.meshgrid(x,y)
+    return np.random.poisson(Expected(x, y, params, a))
+
+def findVar(params, a):
+    """find variance according to eq.54 suppl. Mortensen et. al."""
+    b2 = params[3]**2
+    N = params[4]
+    sa = params[2]
+    F = lambda t: np.log(t) / (1 + (N * a**2 * t / (2 * np.pi * sa**2 * b2)))
+    integral, _ = quad(F, 0, 1)
+    print('integral in variance has value %f' % integral)
+    return sa**2 / N  /(1+integral)
+    
+    
+def logLikelihood(params0, a, im):
+    """the - log of the Fisher Likelihood function for a Gaussian with Poisson statistics.
+    see eq. 49 of suppl Mortensen."""
+    x, y = im.shape
+    x = (np.arange(x)+0.5)*a
+    y = (np.arange(y)+0.5)*a
+    x, y = np.meshgrid(x, y)
+    datafun = lambda params: Expected(x, y, params, a).sum(axis = (0,1)) - (
+        im * np.log(Expected(x, y, params, a))).sum(axis = (0,1))
+    optimizedResult = minimize(datafun, params0, method = 'Nelder-Mead', 
+                           options = {'maxiter':10000,'maxfev':10000, 'fatol':1e-5})
+    return optimizedResult.x
